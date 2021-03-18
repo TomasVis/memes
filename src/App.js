50619b20c25ef67991bbedc7ed7a5ec1f5721ac6
TomasVis <visokiams@gmail.com>
2021-03-18 22:24:35 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { makeUseAxios } from "axios-hooks";
+import loadImage from "./loadImage.js";
+import cx from "classnames";
+
+import "./style.scss";
+
+const NUMBER_OF_MEMES = 256;
+const PROXY_URL = "https://thingproxy.freeboard.io/fetch/";
+const URL = "http://alpha-meme-maker.herokuapp.com/memes/";
+
+const useAxios = makeUseAxios({
+  axios: axios.create({
+    baseURL: PROXY_URL + URL,
+  }),
+});
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 function App() {
+  const [currImg, setCurrImg] = useState([]);
+  const [{ data, loading }, fetch] = useAxios({}, { manual: true });
+
+  const handleFetchMeme = () => {
+    if (!loading) {
+      fetch(getRandomInt(NUMBER_OF_MEMES).toString());
+    }
+  };
+
+  const getImage = async (data) => {
+    try {
+      await loadImage(data.data.image, false);
+      setCurrImg([...currImg, data]);
+    } catch (err) {
+      handleFetchMeme();
+    }
+  };
+
+  useEffect(() => {
+    if (data) {
+      getImage(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      {currImg.length > 0 && (
+        <div className="app-meme-wrapper">
+          <h3 className="top-text">
+            {currImg[currImg.length - 1]?.data?.topText}
+          </h3>
+          <img src={currImg[currImg.length - 1]?.data?.image} alt="meme" />
+          <h3 className="bottom-text">
+            {currImg[currImg.length - 1]?.data?.bottomText}
+          </h3>
+        </div>
+      )}
+      <div className="app-button-wrapper">
+        <button
+          className={cx("app-button", {
+            "button button--loading": loading,
+          })}
+          disabled={loading}
+          onClick={() => {
+            handleFetchMeme();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <span
+            className={cx({
+              "text--green": loading,
+            })}
+          >
+            Generate Meme
+          </span>
+        </button>
+        <button
+          className="app-button "
+          onClick={() => {
+            setCurrImg(
+              currImg.filter((_, index) => index !== currImg.length - 1)
+            );
+          }}
+        >
+          Go Back
+        </button>
+      </div>
     </div>
   );
 }
